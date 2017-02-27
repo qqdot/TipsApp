03e6d0527d07734fb8327f589cea941acdb37846
clai <ClaireLai@claide-macbook-air.local>
2017-02-27 23:43:29 +0800

    Tips APP

--- TipsApp/ViewController.swift
+++ TipsApp/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var priceTextField: UITextField!
+    @IBOutlet weak var tipsTextField: UITextField!
+    @IBOutlet weak var resultTextField: UILabel!
+    @IBOutlet weak var errorTextField: UILabel!
+    
+    @IBAction func submitButton(_ sender: UIButton) {
+        
+        if priceTextField.text == "" || tipsTextField.text == "" {
+            errorTextField.isHidden = false
+            resultTextField.isHidden = true
+        }else{
+            errorTextField.isHidden = true
+            resultTextField.isHidden = false
+            resultTextField.text = String(Double(priceTextField.text!)! * Double(tipsTextField.text!)! * 0.01)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
